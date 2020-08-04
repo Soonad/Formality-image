2dfc07adc597a1ac282afb561a77b5e24bde2889
Maisa <maisa.milena@gmail.com>
2020-08-04 11:59:55 -0300

    Update to use z_index given an image name

--- Parse_Formality.js
+++ Parse_Formality.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 var path = require("path");
 
-function image_to_hex(image_info) {
+function image_to_hex(image_name, image_info) {
   var pixels = image_info.pixels;
   var width  = image_info.width;
   var height = image_info.height;
   // For each pixel, use 6 bytes to write the info
   var b = new Buffer.alloc(pixels.length * 6);
-
+  console.log("z for: ",image_name, z_index(image_name));
   for(var i = 0; i < pixels.length; i++){
     var pixel = pixels[i];
     if(pixel.color.a !== 0){
       b[i*6]   = pixel.x + (128 - (width / 2));
       b[i*6+1] = pixel.y + (128 - (height / 2));
-      b[i*6+2] = 120; // z
+      b[i*6+2] = z_index(image_name); // z
       b[i*6+3] = pixel.color.r;
       b[i*6+4] = pixel.color.g;
       b[i*6+5] = pixel.color.b;
@@ -22,20 +22,39 @@ function image_to_hex(image_info) {
   return b.toString("hex");
 }
 
+// Return the value of the z_index
+const z_index = (image_name) => {
+  var z_index = has_z_index(image_name);
+  return z_index ? z_index.split("z")[1] : 16;
+}
+
+// Return the match of z_index given a Regex if name have it
+const has_z_index = (image_name) => {
+  var match = new RegExp("z+[0-9]");
+  var match_name = match.exec(image_name);
+  return match_name ? match_name[0] : null;
+}
+
+// Get a formatted term_name for the file (without z_index)
+const term_name = (image_name) => {
+  var z_index = has_z_index(image_name);
+  var name = image_name.split("_"+z_index)[0];
+  return name ? name : image_name;
+}
+
+// Content to be on .fm file
 const file_content = (image_name, image_info) => {
-  var hex_content = image_to_hex(image_info);
-  return "Mons.assets."+image_name+": Image3D\n" + 
+  var hex_content = image_to_hex(image_name, image_info);
+  return "Mons.assets."+term_name(image_name)+": Image3D\n" + 
     '  Image3D.parse("'+hex_content+'")';
 }
 
 async function save_fm_file(image_name, content){
-  var path = "./fm_images/"+"Mons.assets."+image_name+".fm";
+  var path = "./fm_images/"+"Mons.assets."+term_name(image_name)+".fm";
   try {
     fs.writeFileSync(path, content);
-    // console.log("Saved "+path);
     return "Saved "+path;
   } catch (e) {
-    // console.log("I couldn't save the file");
     throw e;
   }
 }
--- index.js
+++ index.js
@@ -2,28 +2,29 @@ var Image = require("./Image_util.js");
 var Formality_aux = require("./Parse_Formality.js");
 var fs = require("fs");
 
-function init(){
-  var dirname = "./char/";
+function parse_dir(dirname){
   fs.readdir(dirname, function(err, filenames) {
     if (err) {
       console.log(err);
       return;
     }
-
-    filenames.forEach( image_name => {
-      // Read and get info like pixels formatted, width and height
-      Image.read_image(dirname + image_name)
-      .then( image_info => 
-        // Create a .fm format file and save it in /fm_images
-        Formality_aux.make_fm_file(image_info, image_name)
-        .then ( res => console.log(res) )
-        .catch( err => console.log(err) )
-      )
-      .catch( err => console.log("index.js: got an error: ", err) );
-    });
+    filenames.forEach( image_name => parse_single_image(dirname, image_name));
   })
 }
 
-init();
+// Read and get info like pixels formatted, width and height
+function parse_single_image(dirname, image_name){
+  Image.read_image(dirname + image_name)
+    .then( image_info => 
+      // Create a .fm format file and save it in /fm_images
+      Formality_aux.make_fm_file(image_info, image_name)
+      .then ( res => console.log(res) )
+      .catch( err => console.log(err) )
+    )
+    .catch( err => console.log("index.js: got an error: ", err) );
+}
+
+parse_dir("./16x16/");
+// parse_single_image("./16x16/bush/", "bush_1_z48.png");
 
 // Obs: there is an error with jimp that idk what is, but the code is working
